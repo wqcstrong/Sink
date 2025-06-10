da2e536ee00ab69f97f8bb5f1d10273c920c3418
Blucas <15555602203@163.com>
2025-06-10 19:47:13 +0800

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,12 @@
 <template>
   <main class="flex flex-col justify-center">
+    <!--
     <HomeTwitter />
     <HomeHero />
     <HomeFeatures />
     <HomeLogos />
     <HomeCta />
+    -->
+    <h1>Welcome to come here, there is nothing. Bye<a href="/dashboard">~</a></h1>
   </main>
 </template>
